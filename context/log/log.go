c9e05d2245f5b0c579779ab0e20f4039aa0b5dad
rohini21m <67290441+rohini21m@users.noreply.github.com>
2022-09-05 13:22:23 -0500

    Update log.go

--- context/log/log.go
+++ context/log/log.go
@@ -9,7 +9,7 @@ const reqID = 45
 func Println(ctx context.Context,msg string){
 	id,ok:= ctx,Value(reqId).(int64)
 	if !ok{
-    log.Println("Could not print reqID from conetxt")
+    log.Println("Could not print reqID from context")
 	return 
 	}
     log.Println("%d","%s",id,msg)
